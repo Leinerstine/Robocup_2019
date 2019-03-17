90c77b5ee9e59be9975adbc9ed924608665703b3
Leïly <leilycoquard@users.noreply.github.com>
2019-03-17 17:45:47 +0100

    Update Ultrason.h

--- Robot/tools/Ultrason.h
+++ Robot/tools/Ultrason.h
@@ -1,6 +1,6 @@
 #ifndef _ULTRASON_
 #define _ULTRASON_
-#include "librairies\Accelerometer_And_Gyroscope_LSM6DS3-master\Accelerometer_And_Gyroscope_LSM6DS3-master\SparkFunLSM6DS3.h" 
+#include "librairies\Accelerometer_And_Gyroscope_LSM6DS3-master\SparkFunLSM6DS3.h" 
 
 class Ultrason 
 {
