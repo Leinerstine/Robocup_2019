b9bd36c99dc2a4092d1b6e7be0fa39e81cc586ec
Leïly <leilycoquard@users.noreply.github.com>
2019-03-20 21:03:32 +0100

    update

--- Robot/tools/Temperature.h
+++ Robot/tools/Temperature.h
@@ -8,28 +8,28 @@
 class Temperature
 {
     rgb_lcd         light; //création capteur
-    float           m_temp; //température 
+    float           m_fTemp; //température 
     const int R =   0; //valeurs couleurs
     const int G =   0;
     const int B =   0;
-    bool            m_difference; //verifier si trop grande difference
+    long            m_EndDisplayTime; //temps où éteindre l'affichage
 
     public:
     Temperature()
     {
-        m_temp = 0; //initialisation température
-        m_difference = false; //de base il n'y a pas de différence de température
+        m_fTemp = 0.f; //initialisation température
     }
 
     void Setup()
     {
         light.begin(16,2); //initialisation capteur colonne ligne 
         light.setRGB(R, G, B); //initialisation couleur
+        m_EndDisplayTime = 0;
     }
 
-    float GetTemperature()
+    float GetTemperature() const
     {
-        return m_temp;
+        return m_fTemp;
     }
 
     void Update()
@@ -37,17 +37,31 @@ class Temperature
         float volts = analogRead(_ANALOG_THERMIC); //on obtient la température en volts
         float New_Temp = 20.f - (volts - 25.f)*2.f; //on convertit moyennement en degrés
 
-        if (New_Temp - m_temp > 5) //difference température 5 degrés
-            m_difference = true;
+        if (m_fTemp == 0.f) //initialise
+            m_fTemp = New_Temp;
+
+        if (New_Temp - m_fTemp > 5.f) //difference température 5 degrés
+        {
+           if (m_EndDisplayTime == 0) //pas d'affichage
+           {
+                m_EndDisplayTime = millis() + 5000; //temps actuel + 5 secondes (on coupe dans 5sec)
+                light.setRGB(255,0,0); //on change la couleur background
+                light.setCursor(0,1); //où afficher texte
+                light.print("VICTIM FOUND");
+           }
+        }
+ 
         
-        if(m_difference)
-            light.setRGB(255,0,0); //on change la couleur background
-            light.setCursor(0,1); //où afficher texte
-            light.print("VICTIM FOUND");
+        if(m_EndDisplayTime != 0) //si on affiche
+        {
+            if (m_EndDisplayTime < millis())
+            {
+                light.noDisplay();
+                m_EndDisplayTime = 0;
+            }
+        }
 
-        New_Temp = m_temp; //à la fin de chaque boucle : on modifie la valeur afin de pouvoir comparer avec la dernière valeur 
-
-        delay(100);
+        m_fTemp = New_Temp; //à la fin de chaque boucle : on modifie la valeur afin de pouvoir comparer avec la dernière valeur 
     }
 };
 
