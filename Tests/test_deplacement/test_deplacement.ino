4f60a2ac5878e353b386efd97b53a9208ff51e45
Joktaa <joris.rouziere@gmail.com>
2019-03-28 21:36:33 +0100

    ajout des bibliotheques

--- Tests/test_deplacement/test_deplacement.ino
+++ Tests/test_deplacement/test_deplacement.ino
@@ -1,24 +1,31 @@
-#include "Button.h"
 #include "MoveMotor.h"
 #include "Gyroscope.h"
 #include "Ultrason.h"
+#include "Button.h"
+#include "Temperature.h"
+
 
 //initialise les differents elements
 Gyroscope g_Gyroscope;
 Robot_Move g_Motor; 
 Ultrason g_Ultrason;
 Button g_MainButton(2);
+Button g_MainButton(8);
+Temperature g_Temp;
 
 //variable et pointeur stockant la demande de mouvement
 int needMove = 1;
 int *PneedMove = &needMove;
 bool g_bStop = true; //stock la demande d'arret 
+bool g_bDebugMode = true;
+long Time = 0;
 
 void setup() 
 {
 //lance le moteur, gyroscope et moniteur
     g_Motor.Setup();
-    g_Gyroscope.setup();
+    g_Gyroscope.Setup();
+    g_Temp.Setup();
     Serial.begin(9600);
 }
 
@@ -27,13 +34,45 @@ void loop()
     g_Gyroscope.Update();
     g_Motor.Update(g_Gyroscope.GetRawAngle());
     g_MainButton.Update();
+    g_Ultrason.Update();
+    g_Temp.Update();
 
-    if (g_MainButton.IsPressed()) {
+  if (g_MainButton.IsLongReleased())
+  {
+      if (g_bDebugMode)
+      {
+        g_bDebugMode = false;
+        g_Temp.StopDebug();
+      }
+      else
+      {
+        g_bDebugMode = true;
+      }
+  }
+  if (g_bDebugMode)
+  {
+      char txt1[256];
+      char txt2[256];
+      char txt3[256];
+      long t = g_Temp.GetTemperature();
+      long d = g_Ultrason.GetDistance();
+      long a = g_Gyroscope.GetRawAngle();
+
+      sprintf(txt1, "T %d",t);// D %d A %d", t, d, a);
+      sprintf(txt2, " D %d",d);// D %d A %d", t, d, a);
+      sprintf(txt3, " A %d   ",a);// D %d A %d", t, d, a);
+      strcat(txt1, txt2);
+      strcat(txt1, txt3);
+     g_Temp.DrawDebug(txt1);
+  }
+
+
+    if (g_MainButton.IsShortReleased()) {
         g_bStop = !g_bStop;
     }
 
     if (g_bStop) {
-        g_Motor.ChangeOrder(Robot_Move::STAY, 0, 90);
+        g_Motor.ChangeOrder(Robot_Move::STAY, 0, 0);
     }
     else
     {
@@ -53,14 +92,20 @@ void mfw_analyze(int *PneedMove)
     //AVANCE
     g_Motor.ChangeOrder(Robot_Move::FORWARD, 3000, 0); //avancer 3s
     Serial.print("avance\n");
-    delay(5000);
+    //delay(5000);
+    if(Time > millis())
+      return;
+    Time = millis() + DELTA_TIME;
     
     //ANALYSE AUTOUR DE LUI
     for(i = 0; i<4; i++)
     {
         g_Motor.ChangeOrder(Robot_Move::RIGHT, 0, 90); //tourner 90°
         Serial.print("droite\n");
-        delay(5000);
+        //delay(5000);
+        if(Time > millis())
+          return;
+        Time = millis() + DELTA_TIME;
         g_Ultrason.Update();
         //analyse la distance 
         switch (i)
@@ -100,13 +145,19 @@ void mfw_analyze(int *PneedMove)
             if (l_distance < 10) {
                 g_Motor.ChangeOrder(Robot_Move::LEFT, 0, 180); //tourner 90°
                 Serial.print("demi-tour\n");
-                delay(5000);
+                //delay(5000);
+                if(Time > millis())
+                  return;
+                Time = millis() + DELTA_TIME;
                 *PneedMove = 1;
             }
             else {
                 g_Motor.ChangeOrder(Robot_Move::LEFT, 0, 90); //tourner 90°
                 Serial.print("gauche\n");
-                delay(5000);
+                //delay(5000);
+                if(Time > millis())
+                  return;
+                Time = millis() + DELTA_TIME;
                 *PneedMove = 1;
             }
             
@@ -120,7 +171,11 @@ void mfw_analyze(int *PneedMove)
     else {
         g_Motor.ChangeOrder(Robot_Move::RIGHT, 0, 90); //tourner 90°
         Serial.print("droite\n");
-        delay(5000);
+        //delay(5000);
+        if(Time > millis())
+          return;
+        Time = millis() + DELTA_TIME;
         *PneedMove = 1;
     }
+    
 }
